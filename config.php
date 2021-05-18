90c94630e889203b7ace230a20c54e915b8b8424
Ngoc Nguyen <30312571+neeooneeoon@users.noreply.github.com>
2021-05-18 13:15:29 +0700

    db update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	// connect to database
-	$conn = mysqli_connect("n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "xnkv6z01qb70pk3z", "h53rjcmbusna9v8i", "lgt1wvmddd5fu91w");
+	$conn = mysqli_connect("n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "c0d21bqza09enytv", "khnqoqquzjuyretd", "ou55ukpn13x8i4gt");
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
--- pressy_db.sql
+++ pressy_db.sql
@@ -4,7 +4,7 @@ CREATE TABLE `users` (
 	`id` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`username` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL,
-	`role` enum('Author','Admin') DEFAULT NULL,
+	`role` varchar(255) NOT NULL,
 	`password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -33,12 +33,14 @@ CREATE TABLE `post_category` (
     `cat_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+
 INSERT INTO `users` (`id`, `username`, `email`, `role`, `password`) VALUES
 (1, 'neeooneeoon', 'duyngoc.nguyen2001@gmail.com', 'Administrator', 'neeoon');
 
 INSERT INTO `posts` (`id`, `user_id`, `title`, `slug`, `views`, `thumbnail`, `body`, `published`, `posted_at`) VALUES
-(1, 1, '5 Habits that can improve your life', '5-habits-that-can-improve-your-life', 0, 'banner.jpg', 'Read every day', 1, '2021-02-03 08:58:02'),
-(2, 1, 'Second post on Pressy', 'second-post-on-lifeblog', 0, 'banner.jpg', 'This is the body of the second post on this site', 1, '2021-02-02 14:40:14');
+(1, 1, '5 Habits that can improve your life', '5-habits-that-can-improve-your-life', 0, 'banner.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et enim in erat ornare bibendum. Quisque vitae dictum neque. Integer lacus nulla, commodo at felis at, faucibus lacinia libero. Suspendisse blandit vehicula risus, ac varius felis consequat eget. Maecenas et fringilla metus. Nulla et eros varius, auctor augue nec, elementum nulla. Morbi in ante id ex finibus consequat eget eget arcu. Aliquam tincidunt justo vel nunc euismod, vel bibendum lectus scelerisque. Phasellus in dapibus mauris. Aenean ante ipsum, semper vitae euismod id, tincidunt sit amet justo. Mauris turpis orci, ornare sed ullamcorper sed, placerat quis lectus.', 1, '2021-02-03 08:58:02'),
+(2, 1, 'Second post on Pressy', 'second-post-on-lifeblog', 0, 'banner.jpg', 'Nunc quis nunc ullamcorper, fringilla justo nec, accumsan nulla. Proin consequat lectus sit amet velit eleifend, nec condimentum purus fermentum. Donec eget fermentum enim, imperdiet porta turpis. Sed in venenatis nisl, sit amet posuere risus. Morbi dictum posuere posuere. Aenean ultricies in velit quis pretium. Nulla facilisis laoreet sapien sed varius. Vivamus vel lacus magna. Maecenas iaculis faucibus dolor, in vehicula risus convallis a. Donec in libero enim.', 1, '2021-02-02 14:40:14');
 
 INSERT INTO `categories` (`id`, `name`, `slug`) VALUES
 (1, 'Inspiration', 'inspiration'),
